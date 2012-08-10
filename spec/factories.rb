4108d054252dffcd22e012a0c0e748de718e3fae
Ethan Goldman-Kirst <ballinethan@hotmail.com>
2012-08-09 18:08:36 -0700

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Ethan Goldman-Kirst"
+    email    "ballinethan@hotmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
